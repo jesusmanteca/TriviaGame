10aa3ffee411cf12bdca1b9abafd97434e802eb5
Jesus Manteca <jesusmanteca@gmail.com>
2020-03-04 16:20:59 -0700

    Added the timer and questions to show when start is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,18 @@
+$(document).ready(function() {
+
 $("#startButton").on("click", function() {
-    $("#startButton").hide()
-    setInterval (tic, 1000)
+    $("#startButton").hide();
+    setInterval (tic, 1000);
+    $("#questions").toggle();
 })
 var timer = 120
 function tic () {
     timer = timer - 1
-    $("#timer").text(timer + " Seconds")
+    $("#timer").text(timer + " Seconds ");
 }
+function check(){
+    document.getElementById("afterDone").style.visibility = "visible";
+}
+
+
+})
